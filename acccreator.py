7e01854d97aa7bc7f3f9017ee4c3aad1a435e6aa
BigCodilo <vladgirko1@gmail.com>
2019-09-06 13:12:00 +0300

    some fixes

--- acccreator.py
+++ acccreator.py
@@ -10,7 +10,6 @@ from iroha import IrohaCrypto
 from iroha import Iroha, IrohaGrpc
 
 
-
 class User:
     Name = ""
     Pubkey = ""
@@ -19,11 +18,13 @@ class User:
 
 users = list()
 
+
 class AccCreator:
     def __init__(self):
 
-        # self.IROHA_HOST_ADDR = os.getenv('IROHA_HOST_ADDR', '192.168.88.202')
-        self.IROHA_HOST_ADDR = os.getenv('IROHA_HOST_ADDR', 'localhost')
+        # self.IROHA_HOST_ADDR = os.getenv('IROHA_HOST_ADDR', '192.168.88.62')
+        self.IROHA_HOST_ADDR = os.getenv('IROHA_HOST_ADDR', '192.168.88.202')
+        # self.IROHA_HOST_ADDR = os.getenv('IROHA_HOST_ADDR', 'localhost')
         self.IROHA_PORT = os.getenv('IROHA_PORT', '50051')
         self.admin_private_key = 'f101537e319568c765b2cc89698325604991dca57b9716b58016b253506cab70'
         self.iroha = Iroha('admin@test')
@@ -48,20 +49,17 @@ class AccCreator:
         # for status in self.net.tx_status_stream(transaction):
         #     print(status)
 
-
     def CreateDomainAsset(self):
         commands = [
             self.iroha.command('CreateDomain', domain_id='domain', default_role='user'),
             self.iroha.command('CreateAsset', asset_name='coin',
-                          domain_id='domain', precision=2)
+                               domain_id='domain', precision=2)
         ]
         tx = IrohaCrypto.sign_transaction(
             self.iroha.transaction(commands), self.admin_private_key)
         self.SendTxAndPrintstatus(tx)
 
-
-    #______________________________________________CREATINGUSER____________________________
-
+    # ______________________________________________CREATINGUSER____________________________
 
     def CreateAccount(self, accName):
         user = User()
@@ -70,7 +68,7 @@ class AccCreator:
         user.PubKey = IrohaCrypto.derive_public_key(user.PrivKey)
         tx = self.iroha.transaction([
             self.iroha.command('CreateAccount', account_name=accName, domain_id='domain',
-                          public_key=user.PubKey)
+                               public_key=user.PubKey)
         ])
         IrohaCrypto.sign_transaction(tx, self.admin_private_key)
         self.SendTxAndPrintstatus(tx)
@@ -81,12 +79,12 @@ class AccCreator:
         return ''.join(random.choice(letters) for i in range(stringLength))
 
     def CreateManyAccs(self, amount):
-        for i in range(1, amount):
+        for i in range(0, amount):
             name = self.RandomName()
             self.CreateAccount(name)
 
     def SaveAccsToCSV(self):
-        outfile = open('accounts.csv','w')
+        outfile = open('accounts.csv', 'w')
         writer = csv.writer(outfile)
         writer.writerow(["NAME", "PUBLIC_KEY", "PRIVATE_KEY"])
         for v in users:
@@ -94,18 +92,17 @@ class AccCreator:
 
         outfile.close()
 
-
-    #ADDIINGCOIN
+    # ADDIINGCOIN
 
     def AddAdminCoin(self):
         tx = self.iroha.transaction([
             self.iroha.command('AddAssetQuantity',
-                          asset_id='coin#domain', amount='1000000000.00')
+                               asset_id='coin#domain', amount='1000000000.00')
         ])
         IrohaCrypto.sign_transaction(tx, self.admin_private_key)
         self.SendTxAndPrintstatus(tx)
 
-    #___________TRANSFERING COINS_________________________
+    # ___________TRANSFERING COINS_________________________
 
     def SendToAllAccs(self):
         for user in users:
@@ -114,37 +111,24 @@ class AccCreator:
     def SendToUser(self, user):
         tx = self.iroha.transaction([
             self.iroha.command('TransferAsset', src_account_id='admin@test', dest_account_id=user.Name,
-                          asset_id='coin#domain', description='init top up', amount='100.00')
+                               asset_id='coin#domain', description='init top up', amount='100.00')
         ])
         IrohaCrypto.sign_transaction(tx, self.admin_private_key)
         self.SendTxAndPrintstatus(tx)
 
-#STARTER____________________________________________________
+
+# STARTER____________________________________________________
 
 def main():
-    threadsAmount = input("Amount of threads: ")
-    accountsAmount = input("Amount of accounts: ")
-    accsByThread = int(accountsAmount)/int(threadsAmount)
-
-    startTime = time.time()
-
-    accCreator = AccCreator()
-    for i in range(0, int(threadsAmount)):
-        threading.Thread(target=accCreator.Starting, args = [int(accsByThread)]).start()
-        # accCreator.Starting()
-
-    while threading.activeCount() > 1:
-        time.sleep(1);
-
-    totalTime = startTime - time.time()
-
-    accCreator.SaveAccsToCSV()
-    print("Accounts created: ", accountsAmount)
-    print("Threads: ", threadsAmount)
-    print("For: ", totalTime, " seconds")
-    print("Total transactions: ", accCreator.txAmount)
-
-
+    accounts_amount = input("Amount of accounts: ")
+    start_time = time.time()
+    acc_creator = AccCreator()
+    acc_creator.Starting(int(accounts_amount))
+    total_time = time.time() - start_time
+    acc_creator.SaveAccsToCSV()
+    print("Accounts created: ", accounts_amount)
+    print("For: ", total_time, " seconds")
+    print("Total transactions: ", acc_creator.txAmount)
 
 
 if __name__ == "__main__":
--- test_iroha.py
+++ test_iroha.py
@@ -1,72 +1,143 @@
 import binascii
 import csv
 import os
+import threading
 
 from iroha import IrohaCrypto
 from iroha import Iroha, IrohaGrpc
+import time
+
 
 class User:
     Name = ""
     PubKey = ""
     PrivKey = ""
 
+
 users = list()
 transactions = list()
+irohaTests = list()
+success_txs = 0
+txsStatus = list()
+
 
 class TestIroha:
-    def __init__(self, user):
-        # self.IROHA_HOST_ADDR = os.getenv('IROHA_HOST_ADDR', '192.168.88.202')
-        self.IROHA_HOST_ADDR = os.getenv('IROHA_HOST_ADDR', 'localhost')
-        self.IROHA_PORT = os.getenv('IROHA_PORT', '50051')
+    def __init__(self, user, port="50051", host="localhost"):
+        self.IROHA_HOST_ADDR = os.getenv('IROHA_HOST_ADDR', '192.168.88.202') #andrey
+        # self.IROHA_HOST_ADDR = os.getenv('IROHA_HOST_ADDR', '192.168.88.62') #sergey
+        # self.IROHA_HOST_ADDR = os.getenv('IROHA_HOST_ADDR', host)
+        self.IROHA_PORT = os.getenv('IROHA_PORT', port)
         self.admin_private_key = 'f101537e319568c765b2cc89698325604991dca57b9716b58016b253506cab70'
         self.iroha = Iroha(user.Name)
-        self.net = IrohaGrpc('{}:{}'.format(self.IROHA_HOST_ADDR, self.IROHA_PORT))
-        self.txAmount = 0
+        self.net = IrohaGrpc('{}:{}'.format(self.IROHA_HOST_ADDR, self.IROHA_PORT), 1000)
 
     def SendTx(self, transaction):
         hex_hash = binascii.hexlify(IrohaCrypto.hash(transaction))
         print('Transaction hash = {}, creator = {}'.format(
             hex_hash, transaction.payload.reduced_payload.creator_account_id))
         self.net.send_tx(transaction)
-        self.txAmount += 1
-        for status in self.net.tx_status_stream(transaction):
-            print(status)
+        exit_status = list()
+        # for status in self.net.tx_status_stream(transaction):
+        #     print(status)
+        return exit_status
 
-    def SendToUser(self, fromUser, toUser):
+    def SignTx(self, from_user, to_user):
         tx = self.iroha.transaction([
-            self.iroha.command('TransferAsset', src_account_id=fromUser.Name, dest_account_id=toUser.Name,
-                          asset_id='coin#domain', description='sending', amount='0.01')
+            self.iroha.command('TransferAsset', src_account_id=from_user.Name, dest_account_id=to_user.Name,
+                               asset_id='coin#domain', description='sending', amount='0.01')
         ])
-        IrohaCrypto.sign_transaction(tx, fromUser.PrivKey)
+        IrohaCrypto.sign_transaction(tx, from_user.PrivKey)
         transactions.append(tx)
-        # self.SendTx(tx)
+
+    def GetTxsStatus(self):
+        txsChecked = 0
+        print("Starting checking")
+        for transaction in transactions:
+            txsChecked += 1
+            for status in self.net.tx_status_stream(transaction):
+                txsStatus.append(status)
+            if txsChecked%10 == 0:
+                print(txsChecked, " transaction checked")
+
 
 def ReadCSV():
+    global users
     with open("accounts.csv", "r", newline="") as file:
         reader = csv.reader(file)
-        line = 0
+        first_line = True
         for row in reader:
-            if line != 0:
-                user = User()
-                user.Name = row[0]
-                user.PubKey = row[1][2:len(user.PubKey)-1]
-                user.PrivKey = row[2][2:len(user.PrivKey)-1]
-                users.append(user)
-            line += 1
+            if first_line:
+                first_line = False
+                continue
+            user = User()
+            user.Name = row[0]
+            user.PubKey = row[1][2:len(user.PubKey) - 1]
+            user.PrivKey = row[2][2:len(user.PrivKey) - 1]
+            users.append(user)
+
+# ttwo threads
+
+def FirstHalf():
+    irohaSender = TestIroha(users[1])
+    for tx in transactions[0:int(len(transactions)/2)]:
+        irohaSender.SendTx(tx)
+        global success_txs
+        success_txs += 1
+
+def SecondHalf():
+    irohaSender = TestIroha(users[2])
+    for tx in transactions[int(len(transactions) / 2):int(len(transactions))]:
+        irohaSender.SendTx(tx)
+        global success_txs
+        success_txs += 1
+
+#   end of threading
 
 def main():
     ReadCSV()
+    amount_tx = 0
+    users_length = len(users)
+    for from_user in users[0:int(users_length/ 2)]:
+        iroha_test = TestIroha(from_user)
+        irohaTests.append(iroha_test)
+        for to_user in users[int(users_length/ 2):users_length]:
+            print(" from " + from_user.Name + " to " + to_user.Name)
+            iroha_test.SignTx(from_user, to_user)
+            amount_tx += 1
 
-    for fromUser in range(0, int(len(users)/2)):
-        for toUser in range(int(len(users)/2), len(users)):
-            test = TestIroha(users[fromUser])
-            test.SendToUser(users[fromUser], users[toUser])
 
-    sender = TestIroha(users[3])
-    for tx in transactions:
-        sender.SendTx(tx)
-    # for fromTx in range(1, int((len(transactions)-1)/2)):
-    #     for toTx in range(int((len(transactions)-1)/2), int((len(transactions))):
+    startTime = time.time()
+
+    t1 = threading.Thread(target=FirstHalf)
+    t2 = threading.Thread(target=SecondHalf)
+    t1.start()
+    t2.start()
+    t1.join()
+    t2.join()
+
+    endTime = startTime - time.time()
+    # for iroha_test in irohaTests:
+    #     for tx in transactions[start:end]:
+    #         status = iroha_test.SendTx(tx)
+    #         print(status)
+    #         success_txs += 1
+    #     start = end
+    #     end += txs_per_account
+
+    print("All threads closed")
+    time.sleep(5)
+    txStatusObj = TestIroha(users[1])
+    txStatusObj.GetTxsStatus()
+
+    successesTxs = 0
+    for txStatus in txsStatus:
+        if txStatus[0] == "COMMITTED":
+            successesTxs += 1
+
+
+    print("Total time: ", endTime)
+    print("Total txs: ", success_txs)
+    print("Seccesses txs: ", successesTxs)
 
 
 if __name__ == "__main__":
